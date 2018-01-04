9f15cc3146302ad8e85d36e721ff42638423f243
delaneydemark <ddemark@stuy.edu>
2018-01-03 20:54:50 -0500

    fixed the order of the field and constructor, initialized occasion

--- Article.java
+++ Article.java
@@ -2,23 +2,28 @@ import java.util.ArrayList;
 
 public class Article{
     private String category;
-    private String size;
-    private ArrayList<String> color;
-    private double price;
+    private String size
     private ArrayList<String> occassion;
-    private ArrayList<String> dates;
+    private ArrayList<String> color;
     private String brand;
     private String material;
+    private double price;
+    private ArrayList<String> dates;
     private String fileName;
 
-    public Article(String f, String s, String c, String p, String o, String d,
-		   String b, String m){
-	filename = f;
-	size = s;
-	category = c;
-	price = Double.parseDouble(p);
-	brand = b;
-	material = m;
+    public Article(String ca, String s, String o, String co, String b, String m,
+		   String p, String d, String f){
+		category = ca;
+		size = s;
+		// for now, when the user puts in co,o,d they have to put a space in between
+		String[] occ = o.split("\\s");
+		for (int i = 0; i<occ.length; i++){
+			occasion.add(occ[i]);
+		}
+		brand = b;
+		material = b;
+		price = Double.parseDouble(p);
+		fileName = f; 
 	
     }
 }
