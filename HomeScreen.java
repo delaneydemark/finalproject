e5db14c088a5b500c9251895446169f8d478ac7f
Carmen <cbenitz@stuy.edu>
2018-01-06 14:34:49 -0500

    Created three buttons

--- HomeScreen.java
+++ HomeScreen.java
@@ -5,12 +5,30 @@ import java.io.*;
 
 public class HomeScreen extends Closet implements ActionListener{
   private Container screen;
+  private JButton suggestOutfits, add, filter;
 
   public HomeScreen(){
     //make generic window
     this.setSize(600,400);
     this.setLocation(100,100);
     this.setDefaultCloseOperation(EXIT_ON_CLOSE); // look into this for how to write to file
+
+    screen = this.getContentPane();
+    screen.setLayout(new FlowLayout());
+
+    //create buttons
+    suggestOutfits = new JButton("Suggest Outfits");
+    add = new JButton("Add");
+    filter = new JButton("Filter");
+
+    suggestOutfits.addActionListener(this);
+    add.addActionListener(this);
+    filter.addActionListener(this);
+
+    screen.add(suggestOutfits);
+    screen.add(add);
+    screen.add(filter);
+
   }
 
   public HomeScreen(String filter){
