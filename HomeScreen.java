efa27b64c3567ed5068313d71e93575b8647d1f6
carmenBen <cbenitz@stuy.edu>
2018-01-10 15:07:28 -0500

    HomeScreen now displays images of all articles.

--- HomeScreen.java
+++ HomeScreen.java
@@ -4,12 +4,14 @@ import java.awt.event.*;
 import java.io.*;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import javax.imageio.*;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
 
 public class HomeScreen extends Closet implements ActionListener{
   private Container screen;
   private JButton suggestOutfits, add, filter;
-    private JLabel image;
-    private BufferedImage photo;
 
   public HomeScreen(){
     //make generic window
@@ -32,40 +34,40 @@ public class HomeScreen extends Closet implements ActionListener{
     screen.add(suggestOutfits);
     screen.add(add);
     screen.add(filter);
-    
 
-  }
+    //loop through length of closet and display photos
 
-  public HomeScreen(String filter){
-    //loop through lenght of closet and display photos
-
-      //CREATE AN ARRAY LIST OF JLABELS. AND LOOP THROUGH JLABELS.
-      photo = new BufferedImage();
-      image = new JLabel();
-      ArrayList<JLabel> images = new ArrayList<JLabel>();
-      for(int i = 0;i < clothes.len();i++){
-	  //print the photo of corresponding article
-	  Article art = clothes.get(i);
-	  JLabel  = new JLabel();
-	screen.add();
+    //CREATE AN ARRAY LIST OF JLABELS. AND LOOP THROUGH JLABELS.
+    BufferedImage[]  photos = new BufferedImage[len()];
+    JLabel[] images = new JLabel[len()];
+    for(int i = 0;i < len();i++){
+	//print the photo of corresponding article
+	Article art = get(i);
+	images[i] = new JLabel();
+	screen.add(images[i]);
 	try{
-	    photo = ArticleAddScreen.editImage(120,120,ImageIO.read(new File(art.getFileName())));
-	    ImageIcon i = new ImageIcon(photo);
-	    image.setIcon(i);
+	    photos[i] = ArticleAddScreen.editImage(120,120,ImageIO.read(new File(art.getFileName())));
+	    ImageIcon j = new ImageIcon(photos[i]);
+	    images[i].setIcon(j);
 	}catch(Exception ex){
 	    ex.printStackTrace();
-      }
-  	}
+	}
+    }
+
   }
 
+    public HomeScreen(String filter){
 
-  public void actionPerformed(ActionEvent e){
-    String s = e.getActionCommand();
-    //check if they clicked Add...go to ArticleAddScreen and close current window
-    if(s.equals("Add")){
-      ArticleAddScreen w = new ArticleAddScreen();
-      w.setVisible(true);
-      this.dispose();
+    }
+
+
+    public void actionPerformed(ActionEvent e){
+	String s = e.getActionCommand();
+	//check if they clicked Add...go to ArticleAddScreen and close current window
+	if(s.equals("Add")){
+	    ArticleAddScreen w = new ArticleAddScreen();
+	    w.setVisible(true);
+	    this.dispose();
     }
     //check if they clicked SuggestOutfitScreen...open new window and close current
     if(s.equals("Suggest Outfits")){
